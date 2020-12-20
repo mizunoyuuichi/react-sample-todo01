c603e08d6fce3bfb57dffdb04029bc461dbff766
mizen <45590393+mizunoyuuichi@users.noreply.github.com>
2020-12-20 12:28:23 +0900

    adding

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodos, setIncompleteTodos] = useState(["a", "b"]);
+  const [completeTodos, setCompleteTodos] = useState(["ac", "bc"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
+  let onClickAdd = () => {
+    if (todoText === "") return;
+    setIncompleteTodos([...incompleteTodos, todoText]);
+    setTodoText("");
+  };
+
+  let onClickDelete = (index) => {
+    setIncompleteTodos(
+      [...incompleteTodos].filter((n) => n !== incompleteTodos[index])
+    );
+  };
+
+  return (
+    <>
+      <div className="input-area">
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のリスト</p>
+        <ul>
+          {incompleteTodos.map((todo, index) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のリスト</p>
+        <ul>
+          {completeTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import { App } from "./App";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, document.getElementById("root"));
