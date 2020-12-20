d776d824e04f8fb0944b3ffc8d971f24a330a4ed
mizen <45590393+mizunoyuuichi@users.noreply.github.com>
2020-12-20 15:15:49 +0900

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./styles.css";
+import { InputTodo } from "./components/InputTodo";
 
 export const App = () => {
   let [todoText, setTodoText] = useState("");
@@ -36,14 +37,11 @@ export const App = () => {
 
   return (
     <>
-      <div className="input-area">
-        <input
-          placeholder="TODOを入力"
-          value={todoText}
-          onChange={onChangeTodoText}
-        />
-        <button onClick={onClickAdd}>追加</button>
-      </div>
+      <InputTodo
+        todoText={todoText}
+        onChange={onChangeTodoText}
+        onClick={onClickAdd}
+      />
       <div className="incomplete-area">
         <p className="title">未完了のリスト</p>
         <ul>
---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const InputTodo = (props) => {
+  const { todoText, onChange, onClick } = props;
+  return (
+    <div className="input-area">
+      <input placeholder="TODOを入力" value={todoText} onChange={onChange} />
+      <button onClick={onClick}>追加</button>
+    </div>
+  );
+};
